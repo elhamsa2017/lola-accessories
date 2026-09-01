43d4d981d1f50633cdd6185d234bf767b9a3a60f
elhamsa2017 <elhamsa2017@gmail.com>
2026-09-01 17:33:01 +0300

    test changes

--- products-storage.js
+++ products-storage.js
@@ -3,7 +3,7 @@
         const { data, error } = await supabaseClient
             .from('products')
             .select('id, name, category, price, discount_enabled, discount_price, stock, status, description, imageUrl')
-            .order('name', { ascending: false });
+            .order('name', { ascending: true });
 
         if (error) {
             console.error('Supabase getProducts error:', error);
